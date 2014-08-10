7acd9078cad1c815195cf7f268e6940e3f8a43b8
tyleriguchi <tguchi3@gmail.com>
2014-08-10 16:40:34 -0700

    working on getting two templates rendered and style changes

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 var Recipe = DS.Model.extend({
   title: DS.attr('string'),
+  steps: DS.attr(),
   description: DS.attr('string')
 });
 
@@ -10,12 +11,14 @@ Recipe.reopenClass({
     {
       id: 1,
       title: "Spaghetti and meat",
+      steps: ["Cook noodles", "add meat", "eat the food"],
       description: "ITS NOODLES AND MEAT"
     },
 
     {
       id: 2,
       title: "Gyro",
+      steps: ["get lamb", "eat greek taco"],
       description: "ITS A TACO"
     }
   ]
