d96e821b3a6d7e550500a19003e8439b4555addd
tyleriguchi <tguchi3@gmail.com>
2014-08-13 12:44:10 -0700

    getting step item data, seems hacky

--- app/controllers/recipe/edit.js
+++ app/controllers/recipe/edit.js
@@ -2,12 +2,16 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   actions: {
-    save: function() {
-      var recipe = this.get('model');
-      var steps = recipe.get('steps');
-      
-      recipe.save();
-      this.transitionToRoute('recipe', recipe);
+    save: function(model) {
+      var steps = model.get('steps');
+      var content = steps.get('content').get('content');
+      content.forEach(function(item) {
+        Ember.Logger.debug('item: ', item);
+      });
+      // steps.each(function(item) {
+      //   Ember.Logger.debug('item: ', item);
+      // });
+      // this.transitionToRoute('recipe', recipe);
     }
   }
 });
