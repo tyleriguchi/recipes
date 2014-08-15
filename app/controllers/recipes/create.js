f95d5da6995d96b18b55b26d885af5cd7cfa826e
tyleriguchi <tguchi3@gmail.com>
2014-08-15 15:18:16 -0700

    child model is saving? AWESOME

--- app/controllers/recipes/create.js
+++ app/controllers/recipes/create.js
@@ -1,17 +1,21 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.ArrayController.extend({
   actions: {
     create: function() {
-      var recipe = this.get('model');
-      recipe.get('steps').then(function(steps) {
-        Ember.Logger.debug('steps', steps.get('item'));
-        steps.pushObject(recipe);
-        recipe.save();
-        // this.transitionToRoute('recipe', recipe);
+      var store = this.store;
+      var recipe = store.createRecord('recipe', {
+        title: 'this is a test',
+        description: 'ohhh boy',
+      });
+      var item = store.createRecord('step', {
+        item: 'item'
       });
 
-      // Ember.Logger.debug("recipe: ", newRecipe.get('steps'));
+      recipe.get('steps').then(function(step) {
+        step.pushObject(item);
+      });
+        // this.transitionToRoute('recipe', recipe);
     }
   }
 });
