4830a352566f938edbfe942a1944034d4eb34446
tyleriguchi <tguchi3@gmail.com>
2014-08-17 17:33:52 -0700

    dynamically add step items

--- app/controllers/recipe/edit.js
+++ app/controllers/recipe/edit.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   actions: {
     save: function(model) {
-      var steps = model.get('steps');
-      var content = steps.get('content').get('content');
-      content.forEach(function(item) {
-        Ember.Logger.debug('item: ', item);
-      });
+      // var steps = model.get('steps');
+      // var content = steps.get('content').get('content');
+      // content.forEach(function(item) {
+      //   Ember.Logger.debug('item: ', item);
+      // });
       // steps.each(function(item) {
       //   Ember.Logger.debug('item: ', item);
       // });
--- app/controllers/recipes/create.js
+++ app/controllers/recipes/create.js
@@ -1,21 +1,29 @@
 import Ember from 'ember';
 
-export default Ember.ArrayController.extend({
+export default Ember.ObjectController.extend({
   actions: {
     create: function() {
       var store = this.store;
-      var recipe = store.createRecord('recipe', {
-        title: 'this is a test',
-        description: 'ohhh boy',
+      var title = this.get('title').trim();
+      var description = this.get('description').trim();
+
+      var recipe = this.get('model');
+      recipe.setProperties({
+        'title': title,
+        'description': description
       });
-      var item = store.createRecord('step', {
-        item: 'item'
+      var item = this.get('stepItem');
+      var step = store.createRecord('step', {
+        item: item
       });
 
-      recipe.get('steps').then(function(step) {
-        step.pushObject(item);
+      recipe.get('steps').then(function(result) {
+        result.pushObject(step);
+        result.save();
       });
-        // this.transitionToRoute('recipe', recipe);
+
+      this.transitionToRoute('recipe', recipe);
+
     }
   }
 });
